a12fe7af7c9ceb11be9d84ca55e599cee128aedb
WhyMe <antoine.remy99@gmail.com>
2020-02-06 09:53:11 +0200

    hello

--- push.sh
+++ push.sh
@@ -1,8 +1,9 @@
 git config --global user.email "antoine.remy99@gmail.com";
 git config --global user.name "WhyMe"
 git add *
+echo "Enter commit commentary"
 read a
 git commit -m "$a"
-git push
+git push origin master
 
 
